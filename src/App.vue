c149e6966e8514c95bdd583c6253b52abc31e34b
Joshua Kovalchik <kovee98@gmail.com>
2020-10-18 18:51:50 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                Hello World
+            </h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+            <img alt="Vue logo" src="./assets/logo.png" />
+            <HelloWorld msg="Hello Vue 3.0 + Vite" />
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import HelloWorld from './components/HelloWorld.vue';
+
+    export default defineComponent({
+        name: 'App',
+        components: {
+            HelloWorld
+        }
+    });
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './scss/index.scss';
+
+const app = createApp(App);
+app.mount('#app');
